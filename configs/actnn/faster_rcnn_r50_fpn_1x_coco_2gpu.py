c834a40047b462408380d92998b16e7baa891c41
Dequan Wang <dequanwang@berkeley.edu>
2021-11-12 03:41:12 -0800

    more configs

--- configs/actnn/faster_rcnn_r50_fpn_1x_coco_2gpu.py
+++ configs/actnn/faster_rcnn_r50_fpn_1x_coco_2gpu.py
@@ -1,24 +1,13 @@
 _base_ = [
-    '../_base_/models/retinanet_r50_fpn.py',
+    '../_base_/models/faster_rcnn_r50_fpn.py',
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py'
 ]
-# optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 actnn = True
 data = dict(
     samples_per_gpu=8, # 8*2 = 16
     workers_per_gpu=4,
-)
-optimizer_config = dict(
-    _delete_=True,
-    grad_clip=dict(
-        mode='agc',
-        clip_factor=0.01,
-        eps=1e-3,
-        norm_type=2.0,
-    )
 )
 log_config = dict(
     interval=100,
@@ -29,7 +18,7 @@ log_config = dict(
             init_kwargs=dict(
                 project='detection',
                 entity='actnn',
-                name='retinanet_r50_fpn_1x_coco_2gpu',
+                name='faster_rcnn_r50_fpn_1x_coco_2gpu',
             )
         )
     ]
